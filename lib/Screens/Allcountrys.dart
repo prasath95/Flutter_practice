73806936cf61d8c62a7c0dfc91534f342998552e
prasath95 <6sivaprasath@gmail.com>
2019-12-20 00:47:32 +0530

    county class was created

--- lib/Screens/Allcountrys.dart
+++ lib/Screens/Allcountrys.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class Allcountrys extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Header'),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Header'),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'Screens/Allcountrys.dart';
 
-
-void main()
-{
-  runApp(new MaterialApp(home:Allcountrys(),));
+void main() {
+  runApp(new MaterialApp(
+    home: Allcountrys(),
+  ));
 }
-
